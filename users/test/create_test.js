5ce69ce72344998de8f2ff425b3a5979a41a179a
Koscee <koscee.anason@yahoo.com>
2021-07-19 00:43:58 +0800

    added isNew flag

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -2,13 +2,16 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('saves a user', () => {
+  it('saves a user', (done) => {
     //create new user
     const joe = new User({ name:'Joe' });
 
     //save user
-    joe.save();
-
-    // assert();
+    joe.save()
+      .then(() => {
+        // has joe been saved successfully?
+        assert(!joe.isNew);
+        done();
+      });
   });
 });
