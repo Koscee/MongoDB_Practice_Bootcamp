8f08801d6b1c3c5e4ff3ee6419f4b5c4089bbfb6
Koscee <koscee.anason@yahoo.com>
2021-07-19 01:27:02 +0800

    added promise implementation

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+// tell mongoose to use ES6 Promise implementation
+mongoose.Promise = global.Promise;
+
+// before hook runs only once for all the test
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test', {useNewUrlParser: true, useUnifiedTopology: true});
+  mongoose.connection
+    .once('open', () => {
+      console.log('Connected successfully!'); 
+      done();
+    })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
 
 // empty database before running each test
 beforeEach((done) => {
