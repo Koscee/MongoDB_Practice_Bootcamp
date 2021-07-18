cfe50191973029d132194652a04196b795379f41
Koscee <koscee.anason@yahoo.com>
2021-07-18 13:55:00 +0800

    save a user

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -3,7 +3,12 @@ const User = require('../src/user');
 
 describe('Creating records', () => {
   it('saves a user', () => {
+    //create new user
     const joe = new User({ name:'Joe' });
+
+    //save user
+    joe.save();
+
     // assert();
   });
 });
