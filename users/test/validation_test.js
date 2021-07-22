795631c6034fa4faf5f2764683fdd52393e0da64
Koscee <koscee.anason@yahoo.com>
2021-07-22 22:28:05 +0800

    added validation on name attribute

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -3,6 +3,11 @@ const User = require('../src/user');
 
 describe('Validating records', () => {
   it('requires a username', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    // ES6 destructuring
+    const { message } = validationResult.errors.name;
 
+    assert(message === 'Name is required.');
   });
 });
