da9252d44522bd700adc6fdd49dd843c0d583df4
Koscee <koscee.anason@yahoo.com>
2021-07-22 18:04:18 +0800

    update records via model class

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -28,4 +28,25 @@ describe('Updating records', () => {
   it('A model instance can update', (done) => {
     assertName(joe.update({ name: 'Maggie' }), done);
   });
+
+  it('A model class can update', (done) => {
+    assertName(
+      User.update({ name: 'Joe' }, { name: 'Maggie' }),
+      done
+    );
+  });
+
+  it('A model class can update one record', (done) => {
+    assertName(
+      User.findOneAndUpdate({ name: 'Joe' }, { name: 'Maggie' }),
+      done
+    );
+  });
+
+  it('A model class can find a record with an Id and update', (done) => {
+    assertName(
+      User.findByIdAndUpdate(joe._id, {  name: 'Maggie'}),
+      done
+    );
+  });
 });
