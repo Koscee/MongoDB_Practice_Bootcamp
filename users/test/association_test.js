486ccaab87e278674b67f59c8d7be64082ca4653
Koscee <koscee.anason@yahoo.com>
2021-07-27 23:28:46 +0800

    added deeply nested associations

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -20,12 +20,33 @@ describe('Associations', () => {
       .then(() => done());
   });
 
-
   it('saves a relation between a user and a blogpost', (done) => {
     User.findOne({ name: 'Joe' })
       .populate('blogPosts')
       .then((user) => {
         assert(user.blogPosts[0].title === 'Learning Mongodb');
+        done();
+      }).catch(done);
+  });
+
+  it('saves a full relation tree', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'Learning Mongodb');
+        assert(user.blogPosts[0].comments[0].content === 'Wow! great post');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
         done();
       }).catch(done);
   });
