94e9ac5fbe63760bc07adf30b0ec9ec790b852aa
Koscee <koscee.anason@yahoo.com>
2021-07-27 16:27:25 +0800

    added likes and replaced update test postCount

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 });
+    joe = new User({ name: 'Joe', likes: 0 });
     joe.save()
       .then(() => done());
   });
@@ -50,13 +50,14 @@ describe('Updating records', () => {
     );
   });
 
+// if you want mocha to ommit a test add 'x' to 'it()' = xit()
+
 // applying increment update operator
-// add 'x' to 'it()' = xit(), this tells mocha to ommit a test
-  xit('A user can have their postCount incremented by 1', (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 10 } })
+  it('A user can have their likes incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { likes: 10 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
-        assert(user.postCount === 10);
+        assert(user.likes === 10);
         done();
       }).catch(done);
   });
