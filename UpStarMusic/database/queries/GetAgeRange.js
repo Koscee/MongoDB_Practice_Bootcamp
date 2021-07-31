51c859b6faada8c2350ee2b6f801fee069bd7226
Koscee <koscee.anason@yahoo.com>
2021-08-01 03:19:52 +0800

    min and max range values solution

--- UpStarMusic/database/queries/GetAgeRange.js
+++ UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,33 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  /*
+    * this solution works but the performance will be poor
+    * bec all the artist records are loaded in the server @ ".then" method
+  */
+  // return Artist.find({})
+  //   .sort({ age: 1 })
+  //   .then((artists) => {
+  //     return { min: artists[0].age, max: artists[artists.length - 1].age };
+  //   });
+
+
+  /*
+    * this solution is better than the above solution in performance
+  */
+  function rangeQuery(sortValue) {
+    return Artist
+            .find({})
+            .sort({ age: sortValue })
+            .limit(1)
+            .then(artists => artists[0].age);
+  }
+
+  const minValue = rangeQuery(1);
+  const maxValue = rangeQuery(-1);
+
+  return Promise.all([minValue, maxValue])
+    .then((result) => {
+      return { min: result[0], max: result[1] };
+    });
 };
--- UpStarMusic/database/queries/GetYearsActiveRange.js
+++ UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,33 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  /*
+    * this solution works but the performance will be poor
+    * bec all the artist records are loaded in the server @ ".then" method
+  */
+  // return Artist.find({})
+  //   .sort({ yearsActive: 1 })
+  //   .then((artists) => {
+  //     return { min: artists[0].yearsActive, max: artists[artists.length - 1].yearsActive };
+  //   });
+
+
+  /*
+    * this solution is better than the above solution in performance
+  */
+  function rangeQuery(sortValue) {
+    return Artist
+            .find({})
+            .sort({ yearsActive: sortValue })
+            .limit(1)
+            .then(artists => artists[0].yearsActive);
+  }
+
+  const minValue = rangeQuery(1);
+  const maxValue = rangeQuery(-1);
+
+  return Promise.all([minValue, maxValue])
+    .then((result) => {
+      return { min: result[0], max: result[1] };
+    });
 };
