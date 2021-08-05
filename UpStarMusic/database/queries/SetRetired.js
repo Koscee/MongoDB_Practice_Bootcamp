6ef50c69b0d1678f1c04c06a0cdf73b017eac099
Koscee <koscee.anason@yahoo.com>
2021-08-06 03:32:21 +0800

    added batch update operation on artist record

--- UpStarMusic/database/queries/SetRetired.js
+++ UpStarMusic/database/queries/SetRetired.js
@@ -6,4 +6,15 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves after the update
  */
 module.exports = (_ids) => {
+  return Artist.update(
+    { _id: { $in: _ids } },
+    { retired: true },
+    { multi: true }
+  );
+
+  /* ------- Alternative ------- */
+  // return Artist.updateMany(
+  //   { _id: { $in: _ids } },
+  //   { retired: true }
+  // );
 };
